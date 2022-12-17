2c9187eef594a1a49d7137610161326b7f3c1d81
ThaisAlves7 <75734682+ThaisAlves7@users.noreply.github.com>
2022-12-17 19:36:25 -0300

    Aula 01
    Configuração de passos iniciais do projeto conhecendo as particularidades do framework NestJS integrado ao Prisma como ORM. Iniciado de  banco de dados usando SQLite.

---
+++ src/app.module.ts
@@ -0,0 +1,11 @@
+import { PrismaService } from './prisma.service';
+import { HttpModule } from './http.module';
+import { Module } from '@nestjs/common';
+import { AppController } from './app.controller';
+
+@Module({
+  imports: [HttpModule],
+  controllers: [AppController],
+  providers: [PrismaService],
+})
+export class AppModule {}
